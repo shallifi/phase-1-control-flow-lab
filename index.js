910b113e79c0b2317226e8d53e5a5ed865e2c18a
Benjamin <shallifi@hotmail.com>
2022-03-24 20:49:51 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a) {
+  if (a > 2500) {
+    return `No can do.`
+  }
+  else if (a >= 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (a <= 400) {
+    return `This one is on me!`
+  }
+
+  /*var myFunc;
+  if (num === 0) {
+    myFunc = function(theObject) {
+      theObject.make = 'Toyota';
+    }
+  }*/
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  return city === `NYC`? 'Ok, sounds good.' : `No go.`
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money){
+    case `generous`:
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
+
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(2000));
+console.log(scuberGreetingForFeet(2501));
+console.log(ternaryCheckCity(`NYC`));
+console.log(ternaryCheckCity(`Pittsburgh`))
